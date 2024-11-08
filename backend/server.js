7a92fe706297bcfdd024573ae82d06df12e60c3b
okfiqha <okfiqharisdawati@gmail.com>
2024-11-08 15:27:12 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 db.sequelize.sync()
-  .then(() => {
-    console.log("Database synchronized");
-  })
-  .catch((err) => {
-    console.log("Failed to sync database: " + err.message);
-  });
+    .then(() => {
+        console.log("Database synchronized");
+    })
+    .catch((err) => {
+        console.log("Failed to sync database: " + err.message);
+    });
 
 require("./routes/transaction.routes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
