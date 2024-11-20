e6a0158dd855e48d6484c4a6f009a2a901fe09ff
okfiqha <okfiqharisdawati@gmail.com>
2024-11-20 15:06:56 +0700

    .

--- frontend/src/components/Transaction/TransactionTable.js
+++ frontend/src/components/Transaction/TransactionTable.js
@@ -94,7 +94,7 @@ function TransactionTable() {
   };
 
   return (
-
+<<<<<<< Updated upstream
     <div className="container">
       <div className="row mb-4 ">
         <div className="">
@@ -106,9 +106,10 @@ function TransactionTable() {
       <div className="card mb-4 shadow-lg">
         <div className="card-body">
           <div className="row mb-3">
-    <div class="container">
-    <div class="row mb-4">
-    <div class="col-12">
+=======
+         <div class="container">
+        <div class="row mb-4">
+      <div class="col-12">
     <button className="button" type="submit" onClick={goToTransactionForm} class="btn btn btn-block shadow-lg">
       Tambah Data
     </button>
